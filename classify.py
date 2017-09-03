70662310e68fd27e9bf6d03d908af8424b58d0ac
chrism311 <rojascm02@gmail.com>
2017-09-03 16:20:37 -0500

    recent changes

--- classify.py
+++ classify.py
@@ -1,7 +1,6 @@
 import numpy as np
-from sklearn import ensemble, neighbors, svm, metrics, model_selection, gaussian_process, \
+from sklearn import ensemble, neighbors, svm, metrics, model_selection,\
 	linear_model, tree
-from sklearn.gaussian_process.kernels import RBF
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import sys
@@ -16,16 +15,15 @@ SVM_parameters = {'kernel':('linear','rbf', 'poly'), 'degree':[1,10], 'C':[1,10]
 RF_parameters = {'n_estimators':[1,500], 'max_depth':[1,10], 'min_samples_split':[2,10], 'min_samples_leaf':[1,10]}
 
 clf1 = neighbors.KNeighborsClassifier(n_neighbors= 2)			#k-NN classifier
-clf2 = svm.SVC(gamma=5)						#SVM classifier
+clf2 = svm.SVC(gamma=5)							#SVM classifier
 clf3 = ensemble.RandomForestClassifier(n_estimators=30, max_depth=3)	#Random Forest classifier
-clf4 = linear_model.Perceptron()
 clf5 = ensemble.ExtraTreesClassifier(n_estimators=30, max_depth=3)
 ada_clf = ensemble.AdaBoostClassifier(tree.DecisionTreeClassifier( \
 	max_depth=2), n_estimators=50, algorithm="SAMME.R", learning_rate=0.5)
 grid_SVM = model_selection.GridSearchCV(clf2, SVM_parameters)
 grid_RF = model_selection.GridSearchCV(clf3, RF_parameters)
 vote_clf = ensemble.VotingClassifier(estimators=[('knn',clf1),('svm', clf2), \
-	('rf',clf3),('per', clf4),('tree',clf5), ('ada',ada_clf), ('gSVM',grid_SVM), ('gRF', grid_RF)], voting='hard')
+	('rf',clf3),('tree',clf5), ('ada',ada_clf), ('gSVM',grid_SVM), ('gRF', grid_RF)], voting='hard')
 
 #Function for ROC curve
 probs=[]
@@ -46,7 +44,6 @@ def plot_roc(tpr,fpr,thresholds):
 knn_scores1 = []
 svm_scores1= [] 
 trees_scores1 = []
-gauss_scores1 = []
 
 #k-Fold
 kf = model_selection.KFold(n_splits=5 )				#n_splits=20 yields the best accuracy
@@ -54,15 +51,12 @@ for train , test in kf.split(X):
 	clf1.fit(X[train],y[train])
 	clf2.fit(X[train],y[train])
 	clf3.fit(X[train],y[train])
-	clf4.fit(X[train],y[train])
 	acc_knn1= clf1.score(X[test],y[test])
 	acc_svm1= clf2.score(X[test],y[test])
 	acc_trees1= clf3.score(X[test],y[test])
-	acc_gauss1= clf4.score(X[test],y[test])
 	knn_scores1.append(acc_knn1)
 	svm_scores1.append(acc_svm1)
 	trees_scores1.append(acc_trees1)
-	gauss_scores1.append(acc_gauss1)
 	KNN_cf = metrics.confusion_matrix(y[test], clf1.predict(X[test]))
 	SVM_cf = metrics.confusion_matrix(y[test], clf2.predict(X[test]))
 	RF_cf = metrics.confusion_matrix(y[test], clf3.predict(X[test]))
@@ -71,19 +65,11 @@ print"KNN cf:\n", KNN_cf,"\nSVM cf:\n" ,SVM_cf, "\nRF cf:\n", RF_cf
 print "\nUsing k-fold:\n", "k-NN score: ",100*np.mean(knn_scores1)
 print"SVM score: ", 100*np.mean(svm_scores1)
 print "Random Forest score: ", 100*np.mean(trees_scores1)
-print "Gaussian score:", 100*np.mean(gauss_scores1)
 
 #Scores for Leave One Out CV
 knn_scores2 = []
 svm_scores2 = []
 trees_scores2 = []
-gauss_scores2 = []
-
-#arrays for K-fold scores
-knn_scores1 = []
-svm_scores1= []
-trees_scores1 = []
-gauss_scores1=[]
 
 #Spliting data using Leave One Out cross validation
 #loo = model_selection.LeaveOneOut()
@@ -91,32 +77,30 @@ gauss_scores1=[]
 #	clf1.fit(X[train],y[train])
 #	clf2.fit(X[train],y[train])
 #	clf3.fit(X[train],y[train])
-#	clf4.fit(X[train],y[train])
 #	acc_knn2 = clf1.score(X[test],y[test])
 #	acc_svm2 = clf2.score(X[test],y[test])
 #	acc_trees2 = clf3.score(X[test],y[test])
-#	acc_gauss2= clf4.score(X[test],y[test])
 #	knn_scores2.append(acc_knn2)
 #	svm_scores2.append(acc_svm2)
 #	trees_scores2.append(acc_trees2)
-#	gauss_scores2.append(acc_gauss2)
+#	KNNtemp_pred= clf1.predict_proba(X[test])
 #	temp_pred= clf2.predict_proba(X[test])
+#	RFtemp_pred = clf3.predict_proba(X[test])
 #	probs.append(temp_pred[0,0])
 if (len(sys.argv) < 2):
-    print "Usage: python classify.py <training percentage>"
-    exit()
+	print "Usage: python classify.py <training percentage>"
+	exit()
 
 p=float(sys.argv[1])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-p, random_state=0)
 
-for clf in (clf1, clf2, clf3, clf4, clf5,ada_clf, grid_SVM, grid_RF, vote_clf):
+for clf in (clf1, clf2, clf3, clf5,ada_clf, grid_SVM, grid_RF, vote_clf):
 	clf.fit(X_train,y_train)
 	print clf.__class__.__name__, 100*clf.score(X_test,y_test)
 
 #print "\nUsing Leave One Out:\n","k-nn score:", 100*np.mean(knn_scores2)
 #print "SVM score:",svm_score #100*np.mean(svm_scores2)
 #print "Random forest score:", rf_score #100*np.mean(trees_scores2)
-#print "Gaussian score:", 100*np.mean(gauss_scores2)
 #print "Tree score:", tree_score
 
 #Using cross validation score for the data
@@ -127,11 +111,12 @@ for clf in (clf1, clf2, clf3, clf4, clf5,ada_clf, grid_SVM, grid_RF, vote_clf):
 #print "\nUsing CV metrics:\n","k-NN score:", 100*np.mean(knn_scores3)
 #print "SVM score:",100*np.mean(svm_scores3)
 #print "Random Forest score:",100*np.mean(trees_scores3)
-#print "Gaussian score:", 100*np.mean(gauss_scores3)
 
-#fpr, tpr, thresholds = metrics.roc_curve(y,probs, pos_label=2)
-#plot_roc(tpr,fpr,thresholds)
-#print "AUC: ", metrics.auc(fpr,tpr)
+fpr, tpr, thresholds = metrics.roc_curve(y,probs, pos_label=2)
+
+plot_roc(tpr,fpr,thresholds)
+print "AUC: ", metrics.auc(fpr,tpr)
+
 
 
 
